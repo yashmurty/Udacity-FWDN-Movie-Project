2c5fd2adf0393531a754f7b4abafda638ed3daa9
Yash Murty <yashmurty@gmail.com>
2016-12-11 16:04:38 +0530

    Code Review Updates

--- media.py
+++ media.py
@@ -13,6 +13,9 @@ class Movie():
 
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
+        """__init__ method definition :
+        Same attribute definition as of Movie class level.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
